01a65538f77660cffa44a867a86f11fcdfbef531
Drangello <drangello0505@gmail.com>
2026-05-12 14:08:30 +0200

    feat: implement models and serializers for Offer and OfferDetail management

---
+++ offers_app/api/serializers.py
@@ -0,0 +1,98 @@
+from rest_framework import serializers
+from offers_app.models import Offer, OfferDetail
+from django.db import transaction
+
+class OfferDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OfferDetail
+        fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
+        read_only_fields = ['id']
+
+class OfferDetailUrlSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='offerdetail-detail')
+    
+    class Meta:
+        model = OfferDetail
+        fields = ['id', 'url']
+
+class UserDetailsSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+
+class OfferListSerializer(serializers.ModelSerializer):
+    details = OfferDetailUrlSerializer(many=True, read_only=True)
+    min_price = serializers.SerializerMethodField()
+    min_delivery_time = serializers.SerializerMethodField()
+    user_details = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Offer
+        fields = [
+            'id', 'user', 'title', 'image', 'description', 
+            'created_at', 'updated_at', 'details', 
+            'min_price', 'min_delivery_time', 'user_details'
+        ]
+        
+    def get_min_price(self, obj):
+        details = obj.details.all()
+        return min([d.price for d in details]) if details else 0
+
+    def get_min_delivery_time(self, obj):
+        details = obj.details.all()
+        return min([d.delivery_time_in_days for d in details]) if details else 0
+
+    def get_user_details(self, obj):
+        user = obj.user
+        profile = getattr(user, 'profile', None)
+        return {
+            "first_name": profile.first_name if profile else "",
+            "last_name": profile.last_name if profile else "",
+            "username": user.username
+        }
+
+class OfferCreateUpdateSerializer(serializers.ModelSerializer):
+    details = OfferDetailSerializer(many=True)
+
+    class Meta:
+        model = Offer
+        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details']
+        read_only_fields = ['id', 'user', 'created_at', 'updated_at']
+
+    def validate_details(self, details):
+        if len(details) != 3:
+            raise serializers.ValidationError("An offer must have exactly 3 details.")
+        types = [d.get('offer_type') for d in details]
+        if len(set(types)) != 3 or not all(t in ['basic', 'standard', 'premium'] for t in types):
+            raise serializers.ValidationError("Offer details must include exactly one basic, standard, and premium type.")
+        return details
+
+    @transaction.atomic
+    def create(self, validated_data):
+        details_data = validated_data.pop('details')
+        validated_data['user'] = self.context['request'].user
+        offer = Offer.objects.create(**validated_data)
+        
+        for detail_data in details_data:
+            OfferDetail.objects.create(offer=offer, **detail_data)
+            
+        return offer
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        details_data = validated_data.pop('details', None)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        
+        if details_data:
+            for detail_data in details_data:
+                offer_type = detail_data.get('offer_type')
+                detail_instance = OfferDetail.objects.filter(offer=instance, offer_type=offer_type).first()
+                if detail_instance:
+                    for attr, value in detail_data.items():
+                        setattr(detail_instance, attr, value)
+                    detail_instance.save()
+                    
+        return instance
--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Offer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='offers', verbose_name='Creator')
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='offers/', null=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated_at']
+        verbose_name = 'Offer'
+        verbose_name_plural = 'Offers'
+
+    def __str__(self):
+        return f"{self.title} by {self.user.username}"
+
+
+class OfferDetail(models.Model):
+    class OfferType(models.TextChoices):
+        BASIC = 'basic', 'Basic'
+        STANDARD = 'standard', 'Standard'
+        PREMIUM = 'premium', 'Premium'
+
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name='details')
+    title = models.CharField(max_length=200)
+    revisions = models.IntegerField(default=-1)
+    delivery_time_in_days = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=20, choices=OfferType.choices)
+
+    class Meta:
+        ordering = ['price']
+        verbose_name = 'Offer Detail'
+        verbose_name_plural = 'Offer Details'
+        unique_together = ['offer', 'offer_type']
+
+    def __str__(self):
+        return f"{self.offer.title} - {self.offer_type}"
