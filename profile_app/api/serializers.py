fed74581eca2feae2a77a87c798ac99ca0353226
Drangello <drangello0505@gmail.com>
2026-05-12 14:06:37 +0200

    feat: initialize Profile model and serializer with user profile management logic

---
+++ profile_app/api/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from profile_app.models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.email')
+
+    class Meta:
+        model = Profile
+        fields = [
+            'user',
+            'username',
+            'first_name',
+            'last_name',
+            'file',
+            'location',
+            'tel',
+            'description',
+            'working_hours',
+            'type',
+            'email',
+            'created_at'
+        ]
+        read_only_fields = ['user', 'type', 'created_at']
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+        if 'email' in user_data:
+            instance.user.email = user_data['email']
+            instance.user.save()
+
+        # Update profile fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+            
+        instance.save()
+        return instance
--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    class UserType(models.TextChoices):
+        BUSINESS = 'business', 'Business'
+        CUSTOMER = 'customer', 'Customer'
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile',
+        verbose_name="User"
+    )
+    first_name = models.CharField(max_length=150, blank=True, default='')
+    last_name = models.CharField(max_length=150, blank=True, default='')
+    file = models.ImageField(upload_to='profiles/', blank=True, null=True, verbose_name="Profile Picture")
+    location = models.CharField(max_length=200, blank=True, default='')
+    tel = models.CharField(max_length=50, blank=True, default='')
+    description = models.TextField(blank=True, default='')
+    working_hours = models.CharField(max_length=100, blank=True, default='')
+    type = models.CharField(
+        max_length=20,
+        choices=UserType.choices,
+        default=UserType.CUSTOMER,
+        verbose_name="User Type"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return f"{self.user.username} ({self.type})"
