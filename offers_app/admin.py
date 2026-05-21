486a35eec502e2bec3d14fedd42dec8ddfe33f9b
Drangello <drangello0505@gmail.com>
2026-05-21 16:45:51 +0200

    feat: add Offer and OfferDetail admin configurations with inline support and enhanced display options

--- offers_app/admin.py
+++ offers_app/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Offer, OfferDetail
+
+
+class OfferDetailInline(admin.TabularInline):
+    model = OfferDetail
+    extra = 0
+    min_num = 1
+    fields = (
+        'offer_type',
+        'title',
+        'price',
+        'delivery_time_in_days',
+        'revisions',
+        'features',
+    )
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'user',
+        'detail_count',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'created_at',
+        'updated_at',
+    )
+    search_fields = (
+        'title',
+        'description',
+        'user__username',
+        'user__email',
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+    fieldsets = (
+        ('Angebot', {
+            'fields': (
+                'user',
+                'title',
+                'image',
+                'description',
+            ),
+        }),
+        ('Zeitstempel', {
+            'fields': (
+                'created_at',
+                'updated_at',
+            ),
+            'classes': ('collapse',),
+        }),
+    )
+    inlines = (OfferDetailInline,)
+    autocomplete_fields = ('user',)
+    date_hierarchy = 'updated_at'
+
+    @admin.display(description='Pakete')
+    def detail_count(self, obj):
+        return obj.details.count()
+
+
+@admin.register(OfferDetail)
+class OfferDetailAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'offer',
+        'offer_type',
+        'price',
+        'delivery_time_in_days',
+        'revisions',
+    )
+    list_filter = (
+        'offer_type',
+        'delivery_time_in_days',
+    )
+    search_fields = (
+        'title',
+        'offer__title',
+        'offer__user__username',
+        'offer__user__email',
+    )
+    autocomplete_fields = ('offer',)
