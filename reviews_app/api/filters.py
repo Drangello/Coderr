54564add5980fdd2f5805b41fd64829fe2f7f552
Drangello <drangello0505@gmail.com>
2026-05-24 13:01:16 +0200

    fix: update ReviewFilter field names for consistency with model attributes

--- reviews_app/api/filters.py
+++ reviews_app/api/filters.py
@@ -2,9 +2,9 @@ from django_filters import rest_framework as filters
 from reviews_app.models import Review
 
 class ReviewFilter(filters.FilterSet):
-    business_user_id = filters.NumberFilter(field_name='business_user_id')
-    reviewer_id = filters.NumberFilter(field_name='reviewer_id')
+    business_user = filters.NumberFilter(field_name='business_user')
+    reviewer = filters.NumberFilter(field_name='reviewer')
 
     class Meta:
         model = Review
-        fields = ['business_user_id', 'reviewer_id']
+        fields = ['business_user', 'reviewer']
