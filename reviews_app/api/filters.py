c4e6b4adac6c929fc7cb4c839f2a461bdd6f8e4f
Drangello <drangello0505@gmail.com>
2026-05-26 14:46:34 +0200

    fix: update ReviewFilter field names for consistency and remove pagination from ReviewViewSet

--- reviews_app/api/filters.py
+++ reviews_app/api/filters.py
@@ -2,9 +2,9 @@ from django_filters import rest_framework as filters
 from reviews_app.models import Review
 
 class ReviewFilter(filters.FilterSet):
-    business_user = filters.NumberFilter(field_name='business_user')
-    reviewer = filters.NumberFilter(field_name='reviewer')
+    business_user_id = filters.NumberFilter(field_name='business_user')
+    reviewer_id = filters.NumberFilter(field_name='reviewer')
 
     class Meta:
         model = Review
-        fields = ['business_user', 'reviewer']
+        fields = ['business_user_id', 'reviewer_id']
