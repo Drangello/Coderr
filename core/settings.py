8440c92f9db103f69fd508611d208f10edef347b
Drangello <drangello0505@gmail.com>
2026-06-04 11:40:11 +0200

    fix: update ALLOWED_HOSTS to include api.coderrfw.de and correct IP address

--- core/settings.py
+++ core/settings.py
@@ -22,11 +22,12 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key")
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
 ALLOWED_HOSTS = [
-	"coderrfw.de",
-	"www.coderrfw.de",
-	"34.159.219.34",
-	"localhost",
-	"127.0.0.1",
+    "coderrfw.de",
+    "www.coderrfw.de",
+    "api.coderrfw.de",
+    "34.91.10.155",
+    "localhost",
+    "127.0.0.1",
 ]
 # Application definition ]
 
